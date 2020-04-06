1368abc9fe9ab46121b090069ede46aab6b50e0f
zedonora <timeundtime@gmail.com>
2020-04-07 07:09:46 +0900

    newnote

--- src/NewNote.js
+++ src/NewNote.js
@@ -1,21 +1,75 @@
-import React from "react";
+import React, {useState} from "react";
+import {withRouter} from 'react-router-dom';
+import { useMutation } from "@apollo/react-hooks";
+import gql from 'graphql-tag';
+
+const NEW_NOTE = gql`
+    mutation createNote($title: String! $content: String!) {
+        createNote( input: {title: $title, content: $content}) {
+            _id
+            title
+            content
+            date
+        }
+    }
+`
+
+const NOTES_QUERY = gql`
+    {
+        allNotes {
+            title
+            content
+            _id
+            date
+        }
+    }
+`
+
 const NewNote = () => {
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+
+    const [createNote] = useMutation(NEW_NOTE, {
+        update(
+            cache,
+            {
+                data: {createNote}
+            }
+        ) {
+            const { allNotes } = cache.readQuery({ query: NOTES_QUERY });
+            cache.writeQuery({
+                query: NOTES_QUERY,
+                data: {allNotes: allNotes.concat([createNote])}
+            });
+        }
+    });
+
     return (
         <div className="container m-t-20">
             <h1 className="page-title">New Note</h1>
             <div className="newnote-page m-t-20">
-                <form>
+                <form onSubmit={e=> {
+                    e.preventDefault();
+                    createNote({
+                        variables: {
+                            title,
+                            content,
+                            date: Date.now()
+                        }
+                    });
+                    //history.push("/");
+                }}>
                     <div className="field">
                         <label className="label">Note Title</label>
                         <div className="control">
-                            <input className="input" type="text" placeholder="Note Tite" />
+                            <input className="input" type="text" placeholder="Note Tite" value={title} onChange={e=>setTitle(e.target.value)}/>
                         </div>
                     </div>
                     
                     <div class="field">
                         <label class="label">Note Content</label>
                         <div class="contol">
-                            <textarea class="textarea" rows="10" placeholder="Note Content here..."></textarea>
+                            <textarea class="textarea" rows="10" placeholder="Note Content here..." value={content} onChange={e => setContent(e.target.value)}></textarea>
                         </div>
                     </div>
 
